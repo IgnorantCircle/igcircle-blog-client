99948bfb5620b0074fdb8b4aeaeab37d64f12522
igCircle <df314376@126.com>
2025-08-18 16:11:17 +0800

    style: husky 代码格式化配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	env: {
+		SERVER_API_URL: process.env.SERVER_API_URL,
+	},
+	// 启用详细日志
+	logging: {
+		fetches: {
+			fullUrl: true,
+		},
+	},
+}
 
-export default nextConfig;
+export default nextConfig
